b7746a3e7eb2d59b2740eb85e881c329991ca33a
safiullaansariti <safiulla2016ansari@gmail.com>
2018-02-14 22:15:16 +0530

    Created Controller

---
+++ GitDemo/Controllers/RestaurantController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitDemo.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //
+        // GET: /Restaurant/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
