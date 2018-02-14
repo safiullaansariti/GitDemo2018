3c60204b23a71a7ba27aceabc8cd2a09ccdea206
safiullaansariti <safiulla2016ansari@gmail.com>
2018-02-14 22:04:55 +0530

    Class created

---
+++ GitDemo/Models/Soup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitDemo.Models
+{
+    public class Soup
+    {
+        public string TomaoSoup { get; set; }
+        public string VegCournSoup { get; set; }
+        public string MashroomSoup { get; set; }
+    }
+}
